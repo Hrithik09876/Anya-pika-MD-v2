fa433fd3c8bf635b09e730056dfcbb29b34f73d2
Hrithik09876 <126371101+Hrithik09876@users.noreply.github.com>
2023-04-12 18:35:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,24 +4,24 @@ const chalk = require('chalk')
 
 global.botname = "Queen Anya" 
 global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "𝙋𝙄𝙆𝘼"  
+global.author = "ꫝ𝘳𝓲𝓽ꫝ𝓲𝘬 "  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
 global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
+global.waterMark = "×͜× ꫝ𝘳𝓲𝓽ꫝ𝓲𝘬 " // Your own watermark 
+global.headerText = "Anya by hrithik" // text for header of menu         
 global.DocumentTitle = "MASTER⚔️PIKA" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ownername = "ꫝ𝘳𝓲𝓽ꫝ𝓲�"   // Owner name //        
+global.ownernumber = ['8075325532'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : _.hrxthik.__" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
 global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
+global.state = "Kerala" // your state name
+global.district = "Malappuram" // your district name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
+global.instagramId = "_.hrxthik.__" // Your insta ID
 global.email = "example@exmaple.com" // put your email here
 
 
@@ -29,7 +29,7 @@ global.email = "example@exmaple.com" // put your email here
 global.premium = ['916900******','917355******']  // available soon
 global.sudo = ['916900******','917355******']  // Co - Owners //
 
-global.prefa = ['-']
+global.prefa = ['.']
 
 //--------Random bot name for Info section--------\\
 name16 = "𓄂⍣⃝ 🇦𝚗𝚢𝚊 𓆩×͜×𓆪ꪾ️"
